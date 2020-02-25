44082bc6f9ccff28d2b6e5b5c462978a312e80d4
andres-boltdasarini <lau47@mail.ru>
2020-02-25 17:31:24 +0300

    48

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,36 +1,39 @@
-const SEND_MESSAGE = 'SEND-MESSAGE'
-const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-TEXT-MESSAGE'
+const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY';
+const SEND_MESSAGE = 'SEND_MESSAGE';
 
 let initialState = {
     dialogs: [
-        {id: 1, name: 'Dima'},
-        {id: 2, name: 'Andrey'},
-        {id: 3, name: 'Kolya'},
-        {id: 4, name: 'Ilya'},
-        {id: 5, name: 'Sanya'},
-        {id: 6, name: 'Jeka'},
-        {id: 7, name: 'Semen'},
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrew'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Viktor'},
+        {id: 6, name: 'Valera'}
     ],
-    messag: [
+    messages: [
         {id: 1, message: 'Hi'},
-        {id: 2, message: 'How is your?'},
-        {id: 3, message: 'Yoy'},
+        {id: 2, message: 'How is your it-kamasutra?'},
+        {id: 3, message: 'Yo'},
         {id: 4, message: 'Yo'},
-        {id: 5, message: 'Y'}
+        {id: 5, message: 'Yo'}
     ],
-    newMessageBody: ""
-}
+    newMessageBody: "it"
+};
 
 const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY:
-            state.newMessageBody = action.body;
-            return state;
+            return {
+                ...state,
+                newMessageBody: action.body
+            };
         case SEND_MESSAGE:
             let body = state.newMessageBody;
-            state.newMessageBody = '';
-            state.messag.push({id: 6, message: body});
-            return state;
+            return {
+                ...state,
+                newMessageBody: '',
+                messages: [...state.messages, {id: 6, message: body}]
+            };
         default:
             return state;
     }
@@ -38,6 +41,6 @@ const dialogsReducer = (state = initialState, action) => {
 
 export const sendMessageCreator = () => ({type: SEND_MESSAGE})
 export const updateNewMessageBodyCreator = (body) =>
-    ({ type: UPDATE_NEW_MESSAGE_BODY, body: body })
+    ({type: UPDATE_NEW_MESSAGE_BODY, body: body})
 
 export default dialogsReducer;
