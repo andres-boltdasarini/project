ead0cfc049b03b721533bdde5146657cf8406899
andres-boltdasarini <lau47@mail.ru>
2020-02-25 15:53:51 +0300

    48profile

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,42 +1,45 @@
-
-const ADD_POST = 'ADD-POST'
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 let initialState = {
     posts: [
-        {id: 1, text: 'hi, how are you', LikesCount: 12},
-        {id: 2, text: 'it\'s my first post', LikesCount: 2},
-        {id: 3, text: 'tratata', LikesCount: 1},
+        {id: 1, message: 'Hi, how are you?', likesCount: 12},
+        {id: 2, message: 'It\'s my first post', likesCount: 11},
+        {id: 3, message: 'Blabla', likesCount: 11},
+        {id: 4, message: 'Dada', likesCount: 11}
     ],
-    newPostText: 'it-kama'
-}
+    newPostText: 'it-kamasutra.com'
+};
 
- const profileReducer = (state = initialState, action) => {
+const profileReducer = (state = initialState, action) => {
 
-    if (action.type === 'ADD-POST') {
-        let newPost = {
-            id: 2,
-            text: state.newPostText,
-            likeCount: 0
+    switch(action.type) {
+        case ADD_POST: {
+            let newPost = {
+                id: 5,
+                message: state.newPostText,
+                likesCount: 0
+            };
+            return {
+                ...state,
+                posts: [...state.posts, newPost],
+                newPostText: ''
+            };
         }
-        state.posts.push(newPost)
-        state.newPostText = ''
-
-    } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
-        state.newPostText = action.newText
-
-    }
-    return state
-}
-
-export const addPostActionCreator = () => {
-    return {
-        type: ADD_POST
+        case UPDATE_NEW_POST_TEXT: {
+            return {
+                ...state,
+                newPostText: action.newText
+            }
+        }
+        default:
+            return state;
     }
 }
-export const updateNewPostTextActionCreator = (text) => {
-    return {
-        type: UPDATE_NEW_POST_TEXT, newText: text
-    }
-}
-export default profileReducer
+
+
+export const addPostActionCreator = () => ({type: ADD_POST})
+export const updateNewPostTextActionCreator = (text) =>
+    ({type: UPDATE_NEW_POST_TEXT, newText: text })
+
+export default profileReducer;
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,10 +2,16 @@ import {combineReducers, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 
-let reducers = combineReducers({
-    profilePage:profileReducer,
-    dialogsPage:dialogsReducer
-})
 
-let store = createStore(reducers)
-export default store
+let reducers = combineReducers({
+    profilePage: profileReducer,
+    dialogsPage: dialogsReducer,
+
+});
+
+let store = createStore(reducers);
+
+window.store = store;
+
+
+export default store;
