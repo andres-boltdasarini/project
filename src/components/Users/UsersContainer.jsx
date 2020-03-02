03fbb82dab00cc5bd1d5b1835ee7f0052e2b1907
andres-boltdasarini <lau47@mail.ru>
2020-03-02 08:49:02 +0300

    62b

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -10,7 +10,7 @@ import {
 } from '../../redux/users-reducer';
 import * as axios from 'axios';
 import Users from './Users';
-import Preloader from "../common/Preloader/Preloader";
+import Preloader from "../../common/Preloader/Preloader"
 import {usersAPI} from "../../api/api";
 
 
@@ -21,57 +21,38 @@ class UsersContainer extends React.Component {
 
         usersAPI.getUsers(this.props.currentPage, this.props.pageSize).then(data => {
 
-            this.props.toggleIsFetching(false);
-            this.props.setUsers(data.items);
-            this.props.setTotalUsersCount(data.totalCount);
-        });
+                this.props.toggleIsFetching(false);
+                this.props.setUsers(data.items);
+                this.props.setTotalUsersCount(data.totalCount);
+            });
     }
 
     onPageChanged = (pageNumber) => {
         this.props.setCurrentPage(pageNumber);
         this.props.toggleIsFetching(true);
 
-        import * as axios from "axios";
+        usersAPI.getUsers(pageNumber, this.props.pageSize)
+            .then(data => {
+                this.props.toggleIsFetching(false);
+                this.props.setUsers(data.items);
+            });
+    }
 
-
-        const instance = axios.create({
-            withCredentials: true,
-            baseURL: 'https://social-network.samuraijs.com/api/1.0/',
-            headers:     {
-                "API-KEY": "b1775b2f-c3a5-4509-8dc9-90b5629de7c3"
-            }
-        });
-
-
-        export const usersAPI = {
-            getUsers(currentPage = 1, pageSize = 10) {
-                return instance.get(`users?page=${currentPage}&count=${pageSize}`)
-                    .then(response => {
-                        return response.data;
-                    });
-            }
-        }
-
-        this.props.toggleIsFetching(false);
-        this.props.setUsers(data.items);
-    });
-}
-
-render() {
-    return <>
-        { this.props.isFetching ? <Preloader /> : null }
-        <Users totalUsersCount={this.props.totalUsersCount}
-               pageSize={this.props.pageSize}
-               currentPage={this.props.currentPage}
-               onPageChanged={this.onPageChanged}
-               users={this.props.users}
-               follow={this.props.follow}
-               unfollow={this.props.unfollow}
-               toggleFollowingProgress={this.props.toggleFollowingProgress}
-               followingInProgress={this.props.followingInProgress}
-        />
-    </>
-}
+    render() {
+        return <>
+            { this.props.isFetching ? <Preloader /> : null }
+            <Users totalUsersCount={this.props.totalUsersCount}
+                        pageSize={this.props.pageSize}
+                        currentPage={this.props.currentPage}
+                        onPageChanged={this.onPageChanged}
+                        users={this.props.users}
+                        follow={this.props.follow}
+                        unfollow={this.props.unfollow}
+                        toggleFollowingProgress={this.props.toggleFollowingProgress}
+                        followingInProgress={this.props.followingInProgress}
+             />
+        </>
+    }
 }
 
 let mapStateToProps = (state) => {
