2d88a8fff82d2debd6aa8088aecad9ff211ea867
andres-boltdasarini <lau47@mail.ru>
2020-03-09 10:40:55 +0300

    61

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import s from'./Header.module.css'
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return <header className={s.header}>
         <img src='https://www.pngkey.com/png/full/24-247134_devil-name-logo-png-check-out-devil-name.png' />
+
+        <div className={s.loginBlock}>
+                       { props.isAuth ? props.login
+                           : <NavLink to={'/login'}>Login</NavLink> }
+                    </div>
     </header>
 }
 
