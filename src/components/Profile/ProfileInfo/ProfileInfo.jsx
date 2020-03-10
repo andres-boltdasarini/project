02198544cbf3331fff3f1e18462d7afbb6d302ef
andres-boltdasarini <lau47@mail.ru>
2020-03-10 17:12:37 +0300

    59test

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import s from './ProfileInfo.module.css'
+import Preloader from "../../../common/Preloader/Preloader";
 
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+    if (!props.profile) {
+        return  <Preloader/>
+    }
     return (<div>
             <div>
                 <img src='https://avatars.mds.yandex.net/get-pdb/251121/e0ad3863-7974-4a1a-9098-398badc9f004/s1200'/>
             </div>
             <div className={s.descriptionBlock}>
+                <img src={props.profile.photos.large} />
                 ava + description
             </div>
         </div>
