e4e385b1858db467d137dec3f78747682310bd12
andres-boltdasarini <lau47@mail.ru>
2020-05-07 13:46:23 +0300

    72

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -6,7 +6,8 @@ class ProfileStatus extends React.Component{
         editMode:false,
         title:'Yo'
     }
-    activateEditMode(){
+    activateEditMode = () =>{
+        console.log("this",this)
         this.setState({
             editMode:true
             })}
@@ -18,7 +19,7 @@ class ProfileStatus extends React.Component{
             return (
                 <div>
                     {!this.state.editMode && <div>
-                        <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+                        <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
                     </div>
                     }
                     {this.state.editMode &&
